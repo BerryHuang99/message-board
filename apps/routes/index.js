8ec8cc4b2da5aa27fac16cf673a324fd9764d1b9
BerryHuang99 <berryhuang99@outlook.com>
2017-09-15 23:33:52 +0800

    the main route of the web v0.0.2

--- apps/routes/index.js
+++ apps/routes/index.js
@@ -1,13 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  res.render('index',{ messages: this.messages });
 });
 
-router.get('/test', function(req, res, next) {
-  res.render('test');
+router.post('/leaving-message', function(req, res, next) {
+  messages.push({email: req.param("email"), content: req.param('content'), time: new Date()});
+  res.redirect('/');
 });
 
 module.exports = router;
