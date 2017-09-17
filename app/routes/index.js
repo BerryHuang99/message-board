97f98354eb5f164062a98baf2f2222b1ea02dbd4
BerryHuang99 <berryhuang99@outlook.com>
2017-09-17 22:56:58 +0800

    changed the apps to app

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,11 +5,16 @@ const messages = [];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index',{ messages: this.messages });
+  res.render('index',{ messages: messages });
 });
 
 router.post('/leaving-message', function(req, res, next) {
-  messages.push({email: req.param("email"), content: req.param('content'), time: new Date()});
+  var message = {
+    email: req.param("email"), 
+    content: req.param('content'), 
+    time: new Date()};
+    
+  messages.push(message);
   res.redirect('/');
 });
 
